8113382977e2a83dcc612b2504aa0206fa836c8b
Brett Buddin <brett@buddin.org>
2023-06-13 10:48:12 -0400

    Help formatting.

--- main.go
+++ main.go
@@ -249,14 +249,14 @@ Usage:
 	
 	ucsrename [-y] filename.wav
 
-The program asks a series of questions to build a filename that conforms to UCS
-standards. The source file's file extension is carried forward to the new file.
-Here's the layout of the filename that it produces:
+The program asks a series of questions to build a filename that conforms to UCS standards. The
+source file's file extension is carried forward to the new file. Here's the layout of the filename
+that it produces:
 
 	CatID_FXName_CreatorID_SourceID_UserData.Extention
 
-CatID, FXName, CreatorID and SourceID are required fields. The UserData field is
-optional and can be to specify information not captured by the UCS standard.
+CatID, FXName, CreatorID and SourceID are required fields. The UserData field is optional and can be
+used to specify information not captured by the UCS standard.
 
 The program will prompt you for all fields, but some fields can be skipped by setting one of the
 following environment variables:
